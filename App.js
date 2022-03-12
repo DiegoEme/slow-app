43e7bd6dedc9e27200b151171ffb854a9aa36c2d
Diego <soterg.uhdeia@gmail.com>
2022-03-12 18:35:04 -0500

    Adding Image Picker in ListEditScreen

--- App.js
+++ App.js
@@ -8,29 +8,12 @@ import * as Permissions from "expo-permissions";
 import Screen from "./app/components/Screen";
 import ImageInput from "./app/components/ImageInput";
 import ImageInputList from "./app/components/ImageInputList";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 LogBox.ignoreLogs([
   "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
 ]);
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
-
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri != uri));
-  };
-
-  return (
-    <Screen>
-      <ImageInputList
-        onAddImage={(uri) => handleAdd(uri)}
-        onRemoveImage={(uri) => handleRemove(uri)}
-        imageUris={imageUris}
-      />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
