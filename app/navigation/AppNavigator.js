8efce55fed57a8078099f84f2ab21bca50861cdb
Diego <soterg.uhdeia@gmail.com>
2022-03-16 06:40:24 -0500

    create api layer

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -6,6 +6,7 @@ import ListingEditScreen from "../screens/ListingEditScreen";
 import FeedNavigator from "./FeedNavigator";
 import AccountNavigator from "./AccountNavigator";
 import NewListingButton from "./NewListingButton";
+import routes from "./routes";
 
 const Tab = createBottomTabNavigator();
 
@@ -28,7 +29,7 @@ const AppNavigator = () => (
         headerShown: false,
         tabBarButton: () => (
           <NewListingButton
-            onPress={() => navigation.navigate("ListingEdit")}
+            onPress={() => navigation.navigate(routes.LISTING_EDIT)}
           />
         ),
         tabBarIcon: ({ color, size }) => (
