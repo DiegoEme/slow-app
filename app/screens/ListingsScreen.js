de1b3c7cf32a26c2da129cf18fa0a4839404ad9e
Diego <soterg.uhdeia@gmail.com>
2022-03-16 07:40:48 -0500

    loading animation

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, ActivityIndicator } from "react-native";
 
+import AppButton from "../components/AppButton";
 import Card from "../components/Card";
 import colors from "../config/colors";
 import listingsApi from "../api/listings";
@@ -9,18 +10,39 @@ import Screen from "../components/Screen";
 
 function ListingsScreen({ navigation }) {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     loadListings();
   }, []);
 
   const loadListings = async () => {
+    setLoading(true);
     const response = await listingsApi.getListings();
+    setLoading(false);
+
+    if (!response.ok) return setError(true);
+
+    setError(false);
     setListings(response.data);
   };
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText>Couldn't retrieve the listings</AppText>
+          <AppButton title="Retry" onPress={loadListings} />
+        </>
+      )}
+      {loading && (
+        <ActivityIndicator
+          style={styles.indicator}
+          size="large"
+          color="#A26360"
+        />
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
@@ -38,6 +60,11 @@ function ListingsScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  indicator: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
   screen: {
     padding: 20,
     backgroundColor: colors.lightgrey,
